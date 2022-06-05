ff3f6c4e8c9a1e9803f2831596c249536e3662ab
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-06-05 17:49:45 -0400

    added mongodb client

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import 'dotenv/config';
+import { MongoClient } from 'mongodb'
 
 const app = express()
 const PORT = process.env.PORT || 8000
+const MONGODB_URL = process.env.MONGODB_URL
 
 const players = {
     "Lionel Andrés Messi":{
@@ -35,6 +37,31 @@ app.get('/api/:name', (req, res) => {
     res.json(players[reqName])
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is now running on ${PORT}.`)
-})
+
+// Throws error if no mongodb_url
+
+if (!MONGODB_URL) throw new Error('MONGODB_URL environment variable required')
+
+// Connection URL
+
+const client = new MongoClient(process.env.MONGODB_URL);
+
+async function main() {
+
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db('fm2020');
+    const collection = db.collection('documents');
+
+    // Server starts listening on given port
+    app.listen(PORT, () => {
+        console.log(`Server is now running on ${PORT}.`)
+    })
+
+    return 'Main done';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
--- url.js
+++
@@ -1,1 +0,0 @@
-const url = 'mongodb+srv://tpacun:z!Baqr8MD9DFcV7@fm2020.jgiwe23.mongodb.net/?retryWrites=true&w=majority';
