e0a529abf55046c3a9a081363a9e55b1d528afeb
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-06-05 16:33:05 -0400

    quick clean up

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const mongoose = require('mongoose')
+const url = 'mongodb+srv://tpacun:z!Baqr8MD9DFcV7@fm2020.jgiwe23.mongodb.net/?retryWrites=true&w=majority'
+
 const players = {
     "Lionel Andrés Messi":{
         "Date of birth": "24 June 1987",
@@ -33,5 +36,5 @@ app.get('/api/:name', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is now running on ${PORT}. Go catch it heh.`)
+    console.log(`Server is now running on ${PORT}.`)
 })
